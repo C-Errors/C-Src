f8fc6bdcc8b6557c0dbec2aa9f797694e7c907f4
Criz <crizmo595@gmail.com>
2021-12-13 11:57:47 +0530

    Create book_union.c

---
+++ Practicals/Practical_13/book_union.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+union bookinfo{
+    char name[20];
+    char author[20];
+    int price;
+};
+
+int main()
+{
+    union bookinfo book;
+    book.name;
+    book.author;
+    book.price;
+
+    printf("\nEnter books name: ");
+    scanf("%s", &book.name);
+
+    printf("Books name = %s\n\n" , book.name);
+
+    printf("Enter books author: ");
+    scanf("%s", &book.author);
+
+    printf("Books author = %s\n\n" , book.author);
+
+    printf("Enter books price: ");
+    scanf("%d", &book.price);
+
+    printf("Books price = %d\n\n" , book.price);
+
+    return 0; 
+}
