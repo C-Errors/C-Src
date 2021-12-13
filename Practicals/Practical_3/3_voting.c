14fd7e98b7397ed98b55d2cf4bf2fd779d629c40
Criz <crizmo595@gmail.com>
2021-12-13 09:31:15 +0530

    Update 3_voting.c

--- Practicals/Practical_3/3_voting.c
+++ Practicals/Practical_3/3_voting.c
@@ -9,14 +9,14 @@ void main()
     scanf ("%d", &age);
 
     if(age >= 18 && age < 100){
-        printf("\nYou are elegible for votings as you are above 18", age);
+        printf("\nYou are eligible for votings as you are above 18", age);
     } 
     else if (age > 100){
-        printf("\nYou are elegible for voting, Plese be carefull", age);
+        printf("\nYou are eligible for voting, Plese be carefull", age);
     }
     else if (age < 18 && age > 0){
         int temp = 18 - age;
-        printf("\nYou are not elegible for voting , You have to wait for %d years", temp);
+        printf("\nYou are not eligible for voting , You have to wait for %d years", temp);
     }
     else {
         printf("Wrong Input");
