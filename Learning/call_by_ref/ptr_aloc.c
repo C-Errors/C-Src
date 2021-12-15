6289be7d00387703acfbf0411132d5bdc576cf0f
Criz <crizmo595@gmail.com>
2021-12-15 11:13:29 +0530

    Create ptr_aloc.c

---
+++ Learning/call_by_ref/ptr_aloc.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main()
+{
+
+    int num=10;
+    float temp=12.79;
+    int *ptr=&num;
+    float *tp = &temp;
+
+    printf("Value of ptr = %x",ptr);
+
+    printf("\nValue of ptr after ptr++ = %x",++ptr);
+
+    printf("\n\nValue of tp = %x", tp);
+
+    printf("\nValue of tp after tp++ = %x",++tp);
+    printf("\n\nValue of tp-- = %x", --tp);
+} 
