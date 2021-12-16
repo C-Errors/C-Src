ca844a18edb130df2fd4c6d03086b7d9590a6017
Criz <crizmo595@gmail.com>
2021-12-16 10:33:32 +0530

    Create pragma.c

---
+++ Learning/storage_class/pragma.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+	
+void func1();
+void func2();
+	
+void __attribute__((constructor))func1(); 
+void __attribute__((destructor)) func2(); 
+
+#pragma warn -xxx
+#pragma warn -rvl
+#pragma warn -par
+#pragma warn -rch
+
+void func1()
+{
+	printf("Inside func1()\n");
+}
+	
+void func2()
+{
+	printf("Inside func2()\n");
+}
+	
+int main()
+{
+	printf("Inside main()\n");
+		
+	return 0;
+}
