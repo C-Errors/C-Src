d8d1dee780c631831a75c4f97e98f7313c8234fc
ErenWNL <94686773+ErenWNL@users.noreply.github.com>
2021-12-16 10:00:39 +0530

    Update define_area.c

--- Learning/storage_class/define_area.c
+++ Learning/storage_class/define_area.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-#define clircleArea(r) (3.1415*(r)*(r))
+#define circleArea(r) (3.1415*(r)*(r))
 
 void main()
 {
     const int num = 45;
-    printf("Area is %f", clircleArea(10.0));
+    printf("Area is %f", circleArea(10.0));
 }
