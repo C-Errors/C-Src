07b61681d036dcf8c59b945db611e00b2978b5c6
Criz <crizmo595@gmail.com>
2021-12-16 09:07:33 +0530

    Create b4_static.c

---
+++ Learning/storage_class/b4_static.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+// Before using static variable
+
+void showValue()
+{
+    int counter = 1;
+    printf("\nYou invoked showValue %d time/s", counter);
+    counter++;
+}
+
+void main()
+{
+    showValue();
+    showValue();
+    showValue();
+    printf("\n\n");
+}
