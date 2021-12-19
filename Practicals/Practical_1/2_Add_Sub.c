afa1c16f6ca5c6a1596646fc49499e21be75a652
Criz <crizmo595@gmail.com>
2021-12-19 10:50:27 +0530

    Update 2_Add_Sub.c

--- Practicals/Practical_1/2_Add_Sub.c
+++ Practicals/Practical_1/2_Add_Sub.c
@@ -3,27 +3,27 @@
 void main()
 
 {
-int a,b;//ADD
-printf("ENTER 2 NUMBERS FOR ADDITION:");
-scanf("%d%d",&a,&b);
+    int a,b;
+    printf("ENTER 2 NUMBERS FOR ADDITION:");
+    scanf("%d%d",&a,&b);
 
-printf("THE SUM OF %d AND %d is %d",a,b,(a+b));
+    printf("THE SUM OF %d AND %d is %d",a,b,(a+b));
 
-float n,m;//SUB
-printf("\nENTER 2 NUMBERS FOR SUBTRACTION:");
-scanf("%f%f",&n,&m);
+    float n,m;
+    printf("\nENTER 2 NUMBERS FOR SUBTRACTION:");
+    scanf("%f%f",&n,&m);
 
-printf("THE SUBTRACTION OF %f AND %f is %f",n,m,(n-m));
+    printf("THE SUBTRACTION OF %f AND %f is %f",n,m,(n-m));
 
-float p,q;//multiplication
-printf("\nENTER 2 NUMBERS FOR MULTIPLICATION:");
-scanf("%f%f",&p,&q);
+    float p,q;
+    printf("\nENTER 2 NUMBERS FOR MULTIPLICATION:");
+    scanf("%f%f",&p,&q);
 
-printf("THE PRODUCT OF %f AND %f is %f",p,q,(p*q));
+    printf("THE PRODUCT OF %f AND %f is %f",p,q,(p*q));
 
-int c,d;//Div
-printf("\nENTER 2 NUMBERS FOR DIVISION:");
-scanf("%d%d",&c,&d);
+    int c,d;
+    printf("\nENTER 2 NUMBERS FOR DIVISION:");
+    scanf("%d%d",&c,&d);
 
-printf("THE DIVISION OF %d AND %d is %d",c,d,(c/d));
+    printf("THE DIVISION OF %d AND %d is %d",c,d,(c/d));
 }
