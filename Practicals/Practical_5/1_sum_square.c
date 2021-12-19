3123137de31349943195ce9fb7d9d4bfb08daac2
Criz <crizmo595@gmail.com>
2021-12-19 11:54:02 +0530

    Update 1_sum_square.c

--- Practicals/Practical_5/1_sum_square.c
+++ Practicals/Practical_5/1_sum_square.c
@@ -2,15 +2,21 @@
 
 int main()
 {
-    int n, rem, sum=0;
+    int n, rem, sum = 0 , main;
+
     printf("Enter any number: ");
     scanf("%d", &n);
-    while(n>0)
+
+    while(n > 0)
     {
-        rem = n%10;
+        main = n * n;
+        
+        rem = n % 10;
         sum = sum + rem*rem;
         n = n/10;
     }
     printf("\nSum of Square of Digits = %d", sum);
+
+    printf("\nMain = %d", main);
     return 0;
 }
