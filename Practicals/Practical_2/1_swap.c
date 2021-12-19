f9b6604d02f83cf92d6d042ca1d23ba6356c7849
Criz <crizmo595@gmail.com>
2021-12-19 10:55:36 +0530

    Minor changes

--- Practicals/Practical_2/1_swap.c
+++ Practicals/Practical_2/1_swap.c
@@ -1,18 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 
 void main()
-{
-    
-int a,b;//SWAP
+{  
+    int a,b;
 
-printf("\nENTER 2 NUMBERS: ");
-scanf("%d%d",&a,&b);
+    printf("\nENTER 2 NUMBERS: ");
+    scanf("%d%d",&a,&b);
 
-printf("\nVALUES BEFORE SWAPPING A = %d and B = %d",a,b);
+    printf("\nVALUES BEFORE SWAPPING A = %d and B = %d",a,b);
 
-a = a + b;
-b = a - b;
-a = a - b;
+    //Let a = 10 , b = 20
 
-printf("\nVALUES BEFORE SWAPPING A = %d and B = %d",a,b);
+    a = a + b;    // 10 + 20 = 30 -a
+    b = a - b;    // 10 - 20 = 10 -b Main
+    a = a - b;    // 30 - 10 = 20 -a Main
+
+    printf("\nVALUES BEFORE SWAPPING A = %d and B = %d",a,b);
 }
