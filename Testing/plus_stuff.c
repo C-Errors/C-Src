70fae3228d6990d8a7575256704f3047936555ff
Criz <crizmo595@gmail.com>
2021-12-25 10:49:08 +0530

    Create plus_stuff.c

---
+++ Testing/plus_stuff.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    
+    char const hi = printf("hi how are you");
+
+    char const lmao = 
+        {
+            printf("\n\nno , how is this even working")
+        };
+    printf("%s", hi);
+
+    printf("%s",lmao);
+
+}
