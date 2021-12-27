21d96e8802c1f62ac3f57f13de4eb9d3d97d8ca0
Criz <crizmo595@gmail.com>
2021-12-27 17:29:08 +0530

    jrsdbgknrsgksnrsr

--- Learning/random/datatype.c
+++ Learning/random/datatype.c
@@ -10,14 +10,17 @@ int main()
     num = 5;
     num1 = 2.0;
     num2 = 10;
-    num3 = 100000000000;
+    num3 = 5;
 
     printf("Value of double is %lf\n", num);
     printf("And size of double is %lf\n", sizeof(double));
+
     printf("\nValue of float = %f\n", num1);
     printf("And size of float is %f\n", sizeof(float));
+
     printf("\nValue of long = %ld\n", num2);
     printf("And size of long is %ld\n", sizeof(long));
+
     printf("\nValue of int = %d\n", num3);
     printf("And size of int is %d\n", sizeof(int));
 
