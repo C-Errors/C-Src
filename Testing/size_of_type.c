a123498b9d4a1846ebf7dc9a8358e2c25453aaf2
Criz <crizmo595@gmail.com>
2021-12-30 11:16:49 +0530

    Update size_of_type.c

--- Testing/size_of_type.c
+++ Testing/size_of_type.c
@@ -11,7 +11,6 @@ int main() {
     printf("Size of float: %zu bytes\n", sizeof(floatType));
     printf("Size of double: %lf bytes\n", sizeof(doubleType));
     printf("Size of char: %zu byte\n", sizeof(charType));
-    
 
     printf("Value of double is %lf\n", doubleType);
     printf("And size of double is %lf\n", sizeof(double));
